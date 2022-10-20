9e90708706031e14ef04d18876bf7383e466fcbf
Perumalraja2 <perumalraja2.m@gmail.com>
2022-10-20 17:54:30 +0300

    DAY 4 Task

---
+++ Day 4/JsoncompareObj.js
@@ -0,0 +1,11 @@
+var obj1={name:"Person1",age:"5"}
+var obj2={age:"5",name:"Person1"}
+
+ if((obj1.name)===(obj2.name)&&(obj1.age)===(obj2.age))
+ {
+     console.log("Both are same")
+ }else
+ {
+   console.log("Both are different")  
+ }
+ 
