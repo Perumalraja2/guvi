9d62120ca134390fe9dc1459161f6c23d95c5091
Perumalraja2 <113368078+Perumalraja2@users.noreply.github.com>
2022-11-17 21:35:50 +0300

    Add files via upload

---
+++ Day 17/script.js
@@ -0,0 +1,57 @@
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="10";
+	},1000)
+
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="9";
+	},2000)
+	
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="8";
+	},3000)
+
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="7";
+	},4000)
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="6";
+	},5000)
+
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="5";
+	},6000)
+
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="4";
+	},7000)
+
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="3";
+	},8000)
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="2";
+	},9000)
+
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="1";
+	},10000)
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="0";
+	},11000)
+setTimeout(()=>{
+var result = document.getElementById('hi')
+result.innerHTML="Happy Independence Day";
+	},12000)
+
+check=()=>alert("Please wait")
